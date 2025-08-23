8c7e8b8422f207971fa1c74e6c9b840c31364fd1
danalawati <dana.allawaty@gmail.com>
2025-08-23 23:21:11 +0400

    Edits to App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import ManagerDashboard from "./ManagerDashboard.";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <ManagerDashboard />
+    </div>
+  );
+}
+export default App;
