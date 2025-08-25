50b0d0ef4aad6eba2a04fe7924b00057eba61c76
danalawati <dana.allawaty@gmail.com>
2025-08-25 21:14:08 +0400

    update admin dashboard and add api.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import ManagerDashboard from "./ManagerDashboard.";
 import "./App.css";
-import AdminDashboard from "./AdminDashboard.";
+import AdminDashboard from "./AdminDashboard";
 
 function App() {
   return (
     <div className="app">
-      <ManagerDashboard />
+      <AdminDashboard />
     </div>
   );
 }
